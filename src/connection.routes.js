a50a4697ba332d0fee2822dfa221bac7aa844a3b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-29 16:36:05 +0100

    Add template and use bootstrap 5 in welcome page

--- src/connection.routes.js
+++ src/connection.routes.js
@@ -1,11 +1,48 @@
-const connectionRoutes = (req, res)=>{
-    res.write('<html>');
-    res.write('<head><title>Connection</title></head><body>');
-    res.write('<h2>Connection form:</h2>');
-    res.write('<form method="POST" action="login"><div><label>Login</label><input type="text" name="login"/></div>');
-    res.write('<div><label>Password</label><input type="password" name="password"/></div><button type="submit">Login</button></form>');
-    res.write('</body></html>');
-    res.end();
+const connectionRoutes = (req, res) => {
+    res.end(template);
 };
+
+const template = `
+<html>
+    <head>
+        <title>Connection</title>
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+         <link href="https://getbootstrap.com/docs/5.0/examples/sign-in/signin.css" rel="stylesheet">
+    </head>
+    <style> 
+            .bd-placeholder-img {
+                font-size: 1.125rem;
+                text-anchor: middle;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                user-select: none;
+            }
+
+            @media (min-width: 768px) {
+                .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+                }
+            }
+    </style>
+    <body class="d-flex align-items-center py-4 bg-body-tertiary text-center">
+        <main class="form-signin w-100 m-auto">
+            <form method="POST" action="login">
+                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingInput" name="login">
+                    <label for="floatingInput">Login</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" name="password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                <p class="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+            </form>
+        </main>
+    </body>
+</html>
+`;
 
 exports.connectionRoutes = connectionRoutes;
