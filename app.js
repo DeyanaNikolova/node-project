20615fc42ad503d1b47f34c9bec0d9ea082e76c9
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-29 10:42:01 +0100

    Change server structure

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const http = require('http');
 
-const server = http.createServer((req, res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello World!');
-});
+// const server = http.createServer((req, res) =>{
+//     res.statusCode = 200;
+//     res.setHeader('Content-type', 'text/plain');
+//     res.end('Hello World!');
+// });
 
-const port = 3000;
-const hostName = 'localhost'
+// const port = 3000;
+// const hostName = 'localhost'
 
-server.listen(port, hostName, ()=>{
-    console.log(`Server is running at http://${hostName}:${port}`);  
-})
+// server.listen(port, hostName, ()=>{
+//     console.log(`Server is running at http://${hostName}:${port}`);  
+// })
 
+http.createServer((req, res)=>{
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Welcome Page</title></head>');
+    res.write('<body><h2>Hello from server!</h2></body>');
+    res.write('</html>');
+    res.end();
+    console.log('Server is running');
+}).listen(3000);
