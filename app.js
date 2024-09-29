bdfbbfb6ee4413cc72db684c34fe255c3fd87243
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-29 10:09:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello World!');
+});
+
+const port = 3000;
+const hostName = 'localhost'
+
+server.listen(port, hostName, ()=>{
+    console.log(`Server is running at http://${hostName}:${port}`);  
+})
+
