3ca65f478b2ad2e78ea4057ac2122e982165915f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-29 11:49:09 +0100

    Add routes for login

--- routes.js
+++ routes.js
@@ -1,9 +1,27 @@
-const requestListener = (req, res)=>{
+const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Welcome Page</title></head>');
-    res.write('<body><h2>Hello from server!</h2></body>');
-    res.write('</html>');
+    if (req.url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Welcome Page</title></head>');
+        res.write('<body><h2>Hello from server!</h2></body>');
+        res.write('</html>');
+    } else if (req.url === '/connection') {
+        res.write('<html>');
+        res.write('<head><title>Connection</title></head><body>');
+        res.write('<h2>Connection form:</h2>')
+        res.write('<form method="POST" action="login"><div><label>Login</label><input type="text" name="login"/></div>');
+        res.write('<div><label>Password</label><input type="password" name="password"/></div><button type="submit">Login</button></form>');
+        res.write('</body></html>');
+    } else if (req.url === '/login' && req.method === 'POST') {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+    } else {
+        res.write('<html>');
+        res.write('<head><title>Not Found Page</title></head>');
+        res.write('<body><h2>Page Not Found!</h2></body>');
+        res.write('</html>');
+        res.statusCode = 404;
+    }
     res.end();
 }
 
