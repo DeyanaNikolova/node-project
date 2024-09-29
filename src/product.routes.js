0acd6716307670056a15007bb08bfc81c55f6402
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-09-29 18:10:07 +0100

    Add product template and display post method to create new product

--- src/product.routes.js
+++ src/product.routes.js
@@ -1,8 +1,42 @@
 const productRoutes = (req, res) => {
-    res.end(template);
+    if (req.method === 'POST') {
+        res.statusCode = 201;
+        const data = [];
+        req.on('data', (chunck) => {
+            data.push(chunck);
+        });
+        req.on('end', () => {
+            const parsedData = Buffer.concat(data).toString();
+            products.push(parseProductData(parsedData));
+
+            // if (title && title.length > 2 && password && password.length > 2) {
+            //     res.setHeader('Location', '/product');
+            // } else {
+            //     res.setHeader('Location', '/connection');
+            // }
+            res.end(template());
+        });
+    } else {
+        res.end(template());
+    }
 };
 
-const template  = `
+const parseProductData = parsedData => {
+    const list = parsedData.split('&');
+    const title = list[0].split('=')[1];
+    const price = list[1].split('=')[1];
+    const amount = list[2].split('=')[1];
+
+    return { title, price, amount };
+}
+
+
+const products = [
+    { title: 'T1', price: '12', amount: '3' },
+    { title: 'T2', price: '32', amount: '5' },
+    { title: 'T3', price: '6', amount: '12' }
+];
+const template = () => `
 <html>
     <head>
         <title>Product Page</title>
@@ -19,7 +53,53 @@ const template  = `
             </div>
         </header>
          <div>
-             <h2>Hello from Product Page</h2>
+            <div class="card-group">
+             <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Product</h5>
+                    <form method="POST" action="product">
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Title</label>
+                                <input type="text" class="form-control" id="title" name="title">
+                            </div>
+                            <div class="mb-3">
+                                <label for="price" class="form-label">Price</label>
+                                <input type="number" class="form-control" id="price" name="price">
+                            </div>
+                            <div class="mb-3">
+                                <label for="amount" class="form-label">Amount</label>
+                                <input type="number" class="form-control" id="amount" name="amount">
+                            </div>
+                            <div class="mb-3" style="float: right">
+                                <button type="submit" class="btn btn-success">Save</button>
+                            </div>
+                    </form>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Products List</h5>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Price</th>
+                                <th>Amount</th>
+                            </tr>
+                        </thead>
+                         <tbody>
+                            ${products.map(product => ` 
+                                <tr> 
+                                    <td>${product.title}</td> 
+                                    <td>${product.price}</td> 
+                                    <td>${product.amount}</td> 
+                                </tr>`
+).reduce((e1, e2) => `${e1}${e2}`)}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
         </div>
          <script>
         function login(){
