a4fe4bd2ebf319603eb75f54c7a7dca5fd4eae3f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-06 19:27:56 +0100

    Write and read data from product.json

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(__dirname, '..', '..', 'data', 'products.json');
+
 let products = [
     { title: 'T1', price: '12', amount: '3' },
     { title: 'T2', price: '32', amount: '5' },
@@ -12,32 +17,56 @@ module.exports = class Product {
         this.price = price;
         this.amount = amount;
     }
+    writeFile(product){
+        fs.writeFile(p, JSON.stringify(product), err =>{
+            console.log(err);   
+        }, ()=>{
+            console.log('product saved with success');  
+        });
+    }
+
+    readFile(callback){
+     return fs.readFile(p, (err, fileContent)=>{
+            if(err){
+                console.log(err);   
+                callback([]);
+            } else{
+                callback(JSON.parse(fileContent));
+            }
+        })
+    }
 
     add() {
         const { title, price, amount } = this;
-        if (!products.some(product => product.title === title)) {
-            products.push(title, price, amount);
-        }
+
+        this.readFile(data =>{
+            if (!data.some(product => product.title === title)) {
+                data.push(title, price, amount);
+            }
+            this.writeFile(data);
+        }); 
     }
 
     update() {
         const { title, price, amount } = this;
 
-        for (let p of products) {
-            if (p.title === title) {
-                p.price = price;
-                p.amount = amount;
-                break;
-            }
-        }
+        // for (let p of products) {
+        //     if (p.title === title) {
+        //         p.price = price;
+        //         p.amount = amount;
+        //         break;
+        //     }
+        // }
     }
 
     static delete(title) {
-        products = products.filter(product => product.title !== title);
+        // products = products.filter(product => product.title !== title);
     }
 
-    static getProducts() {
-        return products;
+    static getProducts(calback) {
+        this.readFile(data =>{
+          calback(data);
+        });
     }
 }
 
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const router = express.Router();
 const productController = require('../controllers/product');
+
+const router = express.Router();
 
 router.post('/', productController.addProduct);
 
