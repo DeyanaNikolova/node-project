f6358faa6b84f8855bfae483d702bec7ec3ea9ba
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-06 07:20:44 +0100

    Add examples of middleware

--- src/example.js
+++ src/example.js
@@ -1,1 +1,23 @@
 const express = require('express');
+
+const app = express();
+
+app.use('/first', (req, res, next)=>{
+    console.log('first');
+   // res.end('first');   
+    next();
+});
+
+app.use('/second', (req, res, next)=>{
+    console.log('second');
+    res.end('second');   
+});
+
+app.use((req, res, next)=>{
+    console.log('root');
+    res.end('root');   
+});
+
+app.listen(3000, () => {
+    console.log('Server is running by express.js');   
+});
