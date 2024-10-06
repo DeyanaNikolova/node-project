5ce1ed99aab50f9cdfd698bf8b950a0fc62e9230
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-06 13:15:26 +0100

    Add conditioans to welcome page

--- src/routes/welcome.js
+++ src/routes/welcome.js
@@ -2,8 +2,12 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    res.render('welcome');
+router.get('/', (req, res, next) => {
+    res.render('welcome', {
+        pageTitle: 'Welcome Page',
+        isWelcomePage: true,
+        isProductPage: false,
+    });
 });
 
 
