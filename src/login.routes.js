6568bb2e309713ee517b77469d6f51cc8850b9ba
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-06 08:18:06 +0100

    Convert login routes.js to use data from req.body

--- src/login.routes.js
+++ src/login.routes.js
@@ -1,29 +1,14 @@
 const loginRoutes = (req, res) => {
     if (req.method === 'POST') {
-        res.statusCode = 302;
-        const data = [];
-        req.on('data', (chunck) => {
-            data.push(chunck);
-        });
-        req.on('end', () => {
-            const parsedData = Buffer.concat(data).toString();
-            const { login, password } = parseConnectionData(parsedData);
+        const { login, password } = req.body;
 
-            if (login && login.length > 2 && password && password.length > 2) {
-                res.setHeader('Location', '/product');
-            } else {
-                res.setHeader('Location', '/connection');
-            }
-            res.end();
-        });
+        if (login && login.length > 2 && password && password.length > 2) {
+            res.redirect(302, '/product');
+        } else {
+            res.redirect(302, '/connection');
+        }
+        res.end();
     }
 };
-
-const parseConnectionData = parsedData => {
-    const list = parsedData.split('&');
-    const login = list[0].split('=')[1];
-    const password = list[1].split('=')[1];
-    return { login, password };
-}
 
 exports.loginRoutes = loginRoutes;
