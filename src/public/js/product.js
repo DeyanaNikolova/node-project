e4888d31c69f8fa1c7592654605148cc8e3e0eeb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-06 13:17:07 +0100

    Create product.js and add functionality from product.hbs

--- src/public/js/product.js
+++ src/public/js/product.js
@@ -1,38 +1,38 @@
-function logout(){
+function logout() {
     window.location.href = '/connection';
-    }
+}
 
-    let selectedTitle = null;
+let selectedTitle = null;
 
-    function remove(title){
+function remove(title) {
     selectedTitle = title;
     const http = new XMLHttpRequest();
     http.addEventListener("load", removeListener);
     http.open("DELETE", "/product?title=" + title);
     http.send();
-    }
+}
 
-    function removeListener() {
-   // console.log(this.responseText);
+function removeListener() {
+    // console.log(this.responseText);
 
-   let rows = document.getElementById('productsTableId').rows;
-        for(let i = 0; i < rows.length; i++){
-            if(rows[i].childNodes[1].innerText === selectedTitle){
-                 rows[i].remove();
-            }
+    let rows = document.getElementById('productsTableId').rows;
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].childNodes[1].innerText === selectedTitle) {
+            rows[i].remove();
         }
     }
+}
 
-    function update(title){
-        let rows = document.getElementById('productsTableId').rows;
-        for(let i = 0; i < rows.length; i++){
-            if(rows[i].childNodes[1].innerText === title){
-                document.getElementById('title').value=rows[i].childNodes[1].innerText;
-                document.getElementById('price').value=rows[i].childNodes[3].innerText;
-                document.getElementById('amount').value=rows[i].childNodes[5].innerText;
-            }
+function update(title) {
+    let rows = document.getElementById('productsTableId').rows;
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].childNodes[1].innerText === title) {
+            document.getElementById('title').value = rows[i].childNodes[1].innerText;
+            document.getElementById('price').value = rows[i].childNodes[3].innerText;
+            document.getElementById('amount').value = rows[i].childNodes[5].innerText;
         }
     }
+}
 
-    let postAction = document.getElementById('postActionId');
-    postAction.value = 'update';
+let postAction = document.getElementById('postActionId');
+postAction.value = 'update';
