e0c25e5a7ef3452283c2ce7719c991734eeb17c4
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-06 17:59:25 +0100

    Add example for fs

---
+++ src/example.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(__dirname, '..', 'data', 'hello.txt');
+
+// fs.writeFile(p, 'hello from example.js', err =>{
+//     console.log(err);   
+// }, ()=>{
+//     console.log('fole modified with success');
+    
+// })
+
+fs.readFile(p, (err, fileContent)=>{
+    if(err){
+        console.log(err);   
+    } else{
+        console.log(fileContent.toString());
+    }
+})
