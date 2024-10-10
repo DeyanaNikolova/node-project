2c3fcea5eace6cbd3ecaa3a33907e10bd926851d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-10 19:38:57 +0100

    Add writeFile and resdFile functions

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -3,13 +3,6 @@ const path = require('path');
 
 const productsPath = path.join(__dirname, '..', '..', 'data', 'products.json');
 
-let products = [
-    { title: 'T1', price: '12', amount: '3' },
-    { title: 'T2', price: '32', amount: '5' },
-    { title: 'T3', price: '6', amount: '12' }
-];
-
-
 module.exports = class Product {
 
     constructor(title, price, amount) {
@@ -17,56 +10,59 @@ module.exports = class Product {
         this.price = price;
         this.amount = amount;
     }
-    writeFile(product){
-        fs.writeFile(productsPath, JSON.stringify(product), err =>{
-            console.log(err);   
-        }, ()=>{
-            console.log('product saved with success!');    
-        });
-    }
-
-    readFile(){
-        fs.readFile(p, (err, fileContent)=>{
-            if(err){
-                console.log(err);   
-            } else{
-                console.log(fileContent.toString());
-            }
-        });
-    }
 
     add() {
         const { title, price, amount } = this;
 
-        this.writeFile({ title, price, amount });
-
-        if(!products.some(product => product.title === title)){
-            products.push({ title, price, amount });
-        } 
+        readFile(data =>{
+            if(!data.some(product => product.title === title)){
+                data.push({ title, price, amount });
+            } 
+            writeFile(data);
+        });
     }
 
     update() {
         const { title, price, amount } = this;
 
-        for (let p of products) {
-            if (p.title === title) {
-                p.price = price;
-                p.amount = amount;
-                break;
-            }
-        }
+        // for (let p of products) {
+        //     if (p.title === title) {
+        //         p.price = price;
+        //         p.amount = amount;
+        //         break;
+        //     }
+        // }
     }
 
     static delete(title) {
-         products = products.filter(product => product.title !== title);
+        // products = products.filter(product => product.title !== title);
     }
 
-    static getProducts() {
-        // this.readFile(data =>{
-        //   calback(data);
-        // });
-        return products;
+    static getProducts(callback) {
+        readFile(data =>{
+          callback(data);
+        });
     }
+}
+
+const writeFile = (product) =>{
+    fs.writeFile(productsPath, JSON.stringify(product), err =>{
+        console.log(err);   
+    }, ()=>{
+        console.log('product saved with success!');    
+    });
+}
+
+const readFile = (callback) => {
+
+   return fs.readFile(productsPath, (err, fileContent)=>{
+        if(err){
+            console.log(err);  
+            callback([]);
+        } else{
+            callback(JSON.parse(fileContent));
+        }
+    });
 }
 
 
