38776029e29ab4d2ce9275cd4705b8a929374735
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-13 15:42:17 +0100

    Update product controller

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product-model')
+const Product = require('../models/product-model');
 const { isAuthenticated } = require('../util/auth');
 
 module.exports.getProducts = (req, res) => {
