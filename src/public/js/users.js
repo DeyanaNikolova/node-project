ff60571229d59411993f1ea8dd7ddbfbdfa9f23b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-13 16:38:22 +0100

    Add remove user functionallity

---
+++ src/public/js/users.js
@@ -0,0 +1,35 @@
+let selectedLogin = null;
+
+function remove(login) {
+    selectedLogin = login;
+    const http = new XMLHttpRequest();
+    http.addEventListener("load", removeListener);
+    http.open("DELETE", "/users?login=" + login);
+    http.send();
+}
+
+function removeListener() {
+    // console.log(this.responseText);
+
+    let rows = document.getElementById('usersTableId').rows;
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].childNodes[5].innerText === selectedLogin) {
+            rows[i].remove();
+        }
+    }
+}
+
+function update(login) {
+    let rows = document.getElementById('usersTableId').rows;
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].childNodes[1].innerText === login) {
+            document.getElementById('firstname').value = rows[i].childNodes[1].innerText;
+            document.getElementById('lastname').value = rows[i].childNodes[3].innerText;
+            document.getElementById('login').value = rows[i].childNodes[5].innerText;
+        }
+    }
+    let postAction = document.getElementById('postActionId');
+    console.log(postAction);
+    postAction.value = 'update';
+}
+
