73f7cdf93a2667cd21b6bb2af94c868993badbdc
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-13 16:20:14 +0100

    Add logic for new user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,11 +1,14 @@
+const User = require('../models/user-model');
 const { isAuthenticated } = require('../util/auth');
 
 module.exports.getUsersPage = (req, res) => {
-    res.render('users', {
-        pageTitle: 'Users Page',
-        page: 'users',
-        users: [{firstname: 'Alex', lastname: 'Doe', login: 'alexDoe'}],
-        isAuthenticated: isAuthenticated(req),
+    User.getUsers(data =>{
+        res.render('users', {
+            pageTitle: 'Users Page',
+            page: 'users',
+            users: data,
+            isAuthenticated: isAuthenticated(req),
+        });
     });
 }
 
@@ -14,5 +17,45 @@ module.exports.getUserProfilePage = (req, res) => {
         pageTitle: 'User Profile Page',
         page: '',
         isAuthenticated: isAuthenticated(req),
+    });
+}
+
+
+module.exports.addUser = (req, res) => {
+    const {firstname, lastname, login } = req.body;
+    const user = new User(firstname, lastname, login);
+
+    // if(postAction === 'update'){
+    //     res.statusCode = 200;
+    //     product.update(()=>{
+    //         Product.getProducts(data =>{
+    //             res.render('product', {
+    //                 products: data,
+    //                 pageTitle: 'Products Page',
+    //                 page: 'product',
+    //                 isAuthenticated: isAuthenticated(req),
+    //             });
+    //         });
+    //     });
+    // } else {
+        res.statusCode = 201;
+        user.add(()=>{
+            User.getUsers(data =>{
+                res.render('users', {
+                    users: data,
+                    pageTitle: 'Users Page',
+                    page: 'users', 
+                    isAuthenticated: isAuthenticated(req),
+                });
+            });
+        });
+   // }  
+}
+
+module.exports.deleteUser = (req, res) => {
+    const login = req.url.split('login=')[1];
+    User.delete(login, ()=>{
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('success');
     });
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,13 @@ const express = require('express');
 const usersController = require('../controllers/users');
 
 const router = express.Router();
+
 router.get('/profile', usersController.getUserProfilePage);
+
 router.get('/', usersController.getUsersPage);
+
+router.post('/', usersController.addUser);
+
+router.delete('/', usersController.deleteUser);
 
 exports.usersRoutes = router;
