4053ab4482a8c3f2aa608c530c6cb40ac2f2271c
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-19 14:40:06 +0100

    Check for existing user to allow login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,19 +1,18 @@
 const { isUserExists } = require('../util/auth');
 
-module.exports.signIn = (req, res)=>{
+module.exports.signIn = (req, res) => {
     const { login, password } = req.body;
 
-    if (isUserExists(login)) {
-        res.cookie('isAuthenticated', true);
-        res.cookie('login', login);
-
-        res.redirect(302, '/product');
-    } else {
-        res.cookie('isAuthenticated', false);
-        res.cookie('login', '');
-
-        res.redirect(302, '/connection');
-    }
-
-    res.end();
+    isUserExists(login, isExisting => {
+        if (isExisting) {
+            res.cookie('isAuthenticated', true);
+            res.cookie('login', login);
+            res.redirect(302, '/product');
+        } else {
+            res.cookie('isAuthenticated', false);
+            res.cookie('login', '');
+            res.redirect(302, '/connection');
+        }
+        res.end();
+    });
 }
