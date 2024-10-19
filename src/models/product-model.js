8908bb1390e37852a8f25d42f3b65b3f3158e0b4
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-19 14:59:04 +0100

    Add attribute creterLogin to product-model

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -5,31 +5,33 @@ const productsPath = path.join(__dirname, '..', '..', 'data', 'products.json');
 
 module.exports = class Product {
 
-    constructor(title, price, amount) {
+    constructor(title, price, amount, creatorLogin) {
         this.title = title;
         this.price = price;
         this.amount = amount;
+        this.creatorLogin = creatorLogin;
     }
 
     add(callback) {
-        const { title, price, amount } = this;
+        const { title, price, amount, creatorLogin } = this;
 
         readFile(data =>{
             if(!data.some(product => product.title === title)){
-                data.push({ title, price, amount });
+                data.push({ title, price, amount, creatorLogin });
             } 
             writeFile(data, callback);
         });
     }
 
     update(callback) {
-        const { title, price, amount } = this;
+        const { title, price, amount, creatorLogin } = this;
 
         readFile(data =>{
             for (let p of data) {
                 if (p.title === title) {
                     p.price = price;
                     p.amount = amount;
+                    p.creatorLogin = creatorLogin;
                     break;
                 }
             }
