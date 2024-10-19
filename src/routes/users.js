5343eb7b0975c88286001b903d04ee61975c024a
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-19 13:19:38 +0100

    Update users routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 router.get('/profile', usersController.getUserProfilePage);
 
-router.get('/', usersController.getUsersPage);
-
 router.post('/', usersController.addUser);
 
 router.delete('/', usersController.deleteUser);
+
+router.get('/', usersController.getUsersPage);
 
 exports.usersRoutes = router;
