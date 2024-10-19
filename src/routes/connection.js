25dca1d87abcc175ea616f8763cd1288115a324e
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-19 13:21:25 +0100

    Add lohin cookie

--- src/routes/connection.js
+++ src/routes/connection.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.cookie('isAuthenticated', false);
+    res.cookie('login', '');
+
     res.sendFile(path.join(__dirname, '..', 'views', 'connection.html'));
 });
 
