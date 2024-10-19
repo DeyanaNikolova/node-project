697f86830535b45835f6330398f7b885557d58da
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-19 13:21:58 +0100

    Add login cookie

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -3,9 +3,13 @@ module.exports.signIn = (req, res)=>{
 
     if (login && login.length > 2 && password && password.length > 2) {
         res.cookie('isAuthenticated', true);
+        res.cookie('login', login);
+
         res.redirect(302, '/product');
     } else {
         res.cookie('isAuthenticated', false);
+        res.cookie('login', '');
+
         res.redirect(302, '/connection');
     }
 
