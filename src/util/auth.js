9f3c4f4acba56dc7b658a6132bce6ef08f823f05
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-19 13:20:20 +0100

    Add xtractCoocies function

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,3 +1,13 @@
-module.exports.isAuthenticated = (req)=>{
-    return req.get('Cookie').split('=')[1]==='true';
+module.exports.isAuthenticated = (req) => {
+    return extractCookies(req.get('Cookie')).isAuthenticated === 'true';
+}
+
+const extractCookies = (cookiesStr) => {
+    const cookiesObj = {};
+    cookiesStr.split(';').map(c => c.trim()).forEach(c => {
+        const key = c.split('=')[0];
+        const value = c.split('=')[1];
+        cookiesObj[key] = value;
+    });
+    return cookiesObj;
 }
