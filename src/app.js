478dc2e0eab22ed5196346c7d486feb1e16fd42a
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-20 18:12:21 +0100

    Add new user

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const db = require('./util/database');
+const User = require('./models/user-model');
 
 
 const { welcomeRoutes } = require('./routes/welcome');
@@ -10,6 +11,7 @@ const { loginRoutes } = require('./routes/login');
 const { notFoundRoutes } = require('./routes/not-found');
 const { productRoutes } = require('./routes/product');
 const { usersRoutes } = require('./routes/users');
+const { where } = require('sequelize');
 
 
 const app = express();
@@ -29,7 +31,22 @@ app.use('/users', usersRoutes);
 app.use(welcomeRoutes);
 // app.use('*', notFoundRoutes);
 
+db.sync()
+    .then(() => {
+        return User.findAll({ where: { login: 'mariaPet' } })
+    })
+    .then(users => {
+        if (users && users.length > 0) {
+            return new Promise((resolve, _) => {
+                resolve('User is already existing!');
+            });
+        }
+        return User.create({ login: 'mariaPet', firstname: 'Maria', lastname: 'Petrova', role: 'ADMIN' });
+    })
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000 ...');
+        });
+    })
+    .catch(err => { console.log(err) });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000 ...');
-});
