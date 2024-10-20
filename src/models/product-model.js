84c73d6c6674a684c26098c7a477986a2f8e6d8d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-20 15:30:24 +0100

    Remove old code

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -14,36 +14,3 @@ const Product = db.define('products', {
 
 module.exports = Product;
 
-
-// const productsPath = path.join(__dirname, '..', '..', 'data', 'products.json');
-
-
-// module.exports = class Product {
-
-//     constructor(title, price, amount, creatorLogin) {
-//         this.title = title;
-//         this.price = price;
-//         this.amount = amount;
-//         this.creatorLogin = creatorLogin;
-//     }
-
-//     add() {
-//         const { title, price, amount, creatorLogin } = this;
-//         return db.execute('INSERT INTO products (title, price, amount, creatorLogin) values (?, ?, ?, ?)', [title, price, amount, creatorLogin]);
-//     }
-
-//     update() {
-//         const { title, price, amount } = this;
-//         return db.execute('UPDATE products SET price=?, amount=? WHERE title=?', [price, amount, title]);
-//     }
-
-//     static delete(title) {
-//        return db.execute('DELETE FROM products WHERE title=?', [title]);
-//     }
-
-//     static getProducts(creatorLogin) {
-//         return db.execute('SELECT * FROM products WHERE products.creatorLogin = ?', [creatorLogin]);
-//     }
-// }
-
-
