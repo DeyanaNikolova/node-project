632a5de5cd9065b1327cd637e31a3653dc1db6ad
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-20 14:11:07 +0100

    Use sequelize

--- src/util/database.js
+++ src/util/database.js
@@ -1,11 +1,6 @@
 require('dotenv').config();
+const Sequelize = require('sequelize');
 
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-});
+const db = new Sequelize(`${process.env.DB_DATABASE}, ${process.env.DB_USERNAME}, ${ process.env.DB_PASSWORD}`, {host: process.env.DB_HOST, dialect: process.env.DB_CONNECTION})
 
-module.exports = pool.promise();
+module.exports = db;
