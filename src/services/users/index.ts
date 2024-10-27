bdf2c4ce1df700eb85316dece01d673ca42095a6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-27 17:21:58 +0000

    Add functionality

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,5 +1,10 @@
-import User from '../../models/user-model';
+import User from '../../../models/user';
 
 export function getUserByLogin(login: string): Promise<any> {
-  return User.findAll({ where: { login: login } });
+  return User.findOne({ where: { login: login } });
 }
+
+export function getUserById(userId: number): Promise<any> {
+  return User.findOne({ where: { id: userId } });
+}
+
