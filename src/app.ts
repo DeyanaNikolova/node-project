f2dcba879333ffdb9bdebe84b58d0ea499d999ef
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-10-27 17:28:19 +0000

    Chande db file

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
+/* eslint-disable */
+const db = require('../models/index');
+
 import express from 'express';
 
 import bodyParser from 'body-parser';
 import path from 'path';
-import db from './util/database';
 
 import  welcomeRouter from './routes/welcome';
 import  connectionRouter  from './routes/connection';
 import  loginRouter  from './routes/login';
 import  productRouter  from './routes/products';
 import  usersRouter  from './routes/users';
-
-import User from './models/user-model';
-import Product from './models/product-model';
 
 const app = express();
 
@@ -27,16 +26,7 @@ app.use('/product', productRouter);
 app.use('/users', usersRouter);
 app.use(welcomeRouter);
 
-Product.belongsTo(User);
-
-db.sync()
-    .then(() =>  User.findAll({ where: { login: 'mariaPet' }}))
-    .then((users: any[]) => {
-        if (!users || users.length <= 0) {
-            return User.create({ login: 'mariaPet', firstname: 'Maria', lastname: 'Petrova', role: 'ADMIN' });     
-        }
-        return new Promise((resolve)=>{resolve('User found.');});
-    })
+db.sequelize.sync()
     .then(() => {
         app.listen(3000, () => {
             console.log('Server is running on port 3000 ...');
