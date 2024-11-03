01899176eefa4e9bd786750f4019bd85e6aadd51
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-03 10:32:03 +0000

    Move models out of src

--- models/product.ts
+++ models/product.ts
@@ -13,6 +13,7 @@ class Product extends Model<ProductAttributes> implements ProductAttributes{
   title!: string;
   price!: number;
   amount!: number;
+  userId?: number;
 
   static associate(models: any) {
     Product.belongsTo(models.User);
@@ -24,7 +25,8 @@ module.exports.init = (sequelize: any) => {
   Product.init({
     title: DataTypes.STRING,
     price: DataTypes.DOUBLE,
-    amount: DataTypes.INTEGER
+    amount: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Product',
