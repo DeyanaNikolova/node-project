51079c7a86d768fea510591aef7d2adaa8645f68
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-09 21:46:52 +0000

    Return user id

--- src/controllers/login/index.ts
+++ src/controllers/login/index.ts
@@ -1,22 +1,14 @@
-import { isUserExists } from '../../util/auth';
 import { Request, Response } from 'express';
+import { isUserExists } from '../../util/auth';
 
 export function signIn(req: Request, res: Response): void{
     const { login } = req.body;
 
     isUserExists(login, (isExisting: boolean, userId?: number) => {
         if (isExisting) {
-            res.cookie('isAuthenticated', true);
-            res.cookie('userId', userId);
-            res.json({success: true});
-          //  res.redirect(302, '/product');
+            res.json({userId: userId, isAuthenticated: true});
         } else {
-            res.cookie('isAuthenticated', false);
-            res.cookie('userId', '');
-
-            res.status(302).json({success: false});
-
-          //  res.redirect(302, '/connection');
+            res.status(302).json({userId: null, isAuthenticated: false});
         }
     });
 }
