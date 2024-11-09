07cbecd885a3e7871a7a4708b5a6e0f2940db4eb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-09 22:46:32 +0000

    Set item in session storige when login and logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,10 +18,18 @@ constructor(private router: Router) {
 }
 
   login(){
+    sessionStorage.setItem('isAuthenticated', 'false');
+    sessionStorage.setItem('userId', '');
     this.router.navigate(['/connection']);
   }
 
   logout(){
+    sessionStorage.setItem('isAuthenticated', 'false');
+    sessionStorage.setItem('userId', '');
     this.router.navigate(['/connection']);
+  }
+
+  isAuthenticated(){
+    return sessionStorage.getItem('isAuthenticated') === 'true';
   }
 }
