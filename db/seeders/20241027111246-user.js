3159bce03ffe8df55628721682712eb3c41710c3
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-10 13:21:50 +0000

    Update seeders and migrations

--- db/seeders/20241027111246-user.js
+++ db/seeders/20241027111246-user.js
@@ -1,11 +1,11 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
 
      await queryInterface.bulkInsert('Users', [
       {
+        id: 1,
       login: 'mariaPet',
       firstName: 'Maria',
       lastName: 'Petrova',
@@ -14,6 +14,7 @@ module.exports = {
       updatedAt: new Date(),
       },
       {
+        id: 2,
         login: 'goshoG',
         firstName: 'Gosho',
         lastName: 'Goshev',
