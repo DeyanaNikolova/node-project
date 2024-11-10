8e6178584cbf0bda9a9a0dd0c5f3f9c224a825ce
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-10 13:23:13 +0000

    Iplements OnInit

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
+  products: any[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+  this.getProducts();
+  }
+
+  remove(title: string):void{
+    this.productService.remove(title).subscribe(() =>{
+      this.getProducts();
+    });
+  }
+
+  update(product: Product){
+    this.productService.update(product).subscribe(products =>{
+      this.products = products;
+    });
+  }
+
+
+  private getProducts(){
+    this.productService.getProducts().subscribe(products =>{
+      this.products = products;
+    });
+  }
 }
