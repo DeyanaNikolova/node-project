a955e4ba27a85f1e05bd12c22dca73e1cf21f019
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-10 13:23:43 +0000

    Add get isAdmin

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
@@ -12,26 +12,34 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
 constructor(
   private router: Router,
   private authService: AuthService,
-) {
+) {}
 
+ngOnInit(): void {
+  
 }
 
+userProfile(){}
+
   login(){
-    this.authService.setAuthConf({userId: 0, isAuthenticated: false});
+    this.authService.setAuthConf({userId: 0, isAuthenticated: false, isAdmin: false});
     this.router.navigate(['/connection']);
   }
 
   logout(){
-    this.authService.setAuthConf({userId: 0, isAuthenticated: false});
+    this.authService.setAuthConf({userId: 0, isAuthenticated: false, isAdmin: false});
     this.router.navigate(['/connection']);
   }
 
   get isAuthenticated(){
     return this.authService.isAuthenticated();
+  }
+
+  get isAdmin(){
+    return this.authService.isAdmin();
   }
 }
