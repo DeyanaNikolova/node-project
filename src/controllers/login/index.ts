3186332c447ec2b03eeba75f93e2d73ed4950163
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-17 16:14:46 +0000

    Add userId and isAuthenticated to login controller

--- src/controllers/login/index.ts
+++ src/controllers/login/index.ts
@@ -6,15 +6,9 @@ export function signIn(req: Request, res: Response): void{
 
     isUserExists(login, (isExisting: boolean, userId?: number) => {
         if (isExisting) {
-           res.cookie('isAuthenticated', true);
-           res.cookie('userId', userId);
-           res.json({success: true});
+           res.json({userId: userId, isAuthenticated: true});
         } else {
-            res.cookie('isAuthenticated', false);
-            res.cookie('userId', '');
-            res.status(302).json({success: false});
-
-            // res.status(302).json({userId: null, isAuthenticated: false});
+           res.status(302).json({userId: null, isAuthenticated: false});
         }
     });
 }
