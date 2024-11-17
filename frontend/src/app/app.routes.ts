e9f979615a9745b43b024b6299e8aeba872a75ac
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-17 10:04:34 +0000

    Add path to user-profile

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ConnectionComponent } from './components/connection/connection.componen
 import { HomeComponent } from './components/home/home.component';
 import { ProductComponent } from './components/product/product.component';
 import { UserComponent } from './components/user/user.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 export const routes: Routes = [
     {path: 'connection', component: ConnectionComponent},
     {path: 'products', component: ProductComponent},
     {path: 'users', component: UserComponent},
+    {path: 'user-profile', component: UserProfileComponent},
     {path: '', component: HomeComponent},
 ];
