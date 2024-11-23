ebfb97f22ce70b48db04ccbb56d268fd432bbfa6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-23 14:06:42 +0000

    Add product type

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -12,7 +12,7 @@ import { ProductService } from '../../services/product.service';
 })
 export class ProductComponent implements OnInit {
 
-  products: any[] = [];
+  products: Product[] = [];
 
   constructor(private productService: ProductService) {}
 
