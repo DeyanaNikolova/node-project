c9326191e667636d337ddd8f48e9b53a4dda9e4f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-24 19:22:45 +0000

    Update interceptor

--- frontend/src/app/http-interceptor.interceptor.ts
+++ frontend/src/app/http-interceptor.interceptor.ts
@@ -3,10 +3,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   const userId = sessionStorage.getItem('userId');
   const cookie =  document.cookie = `userId=${userId}`;
-
+  
   const req2 = req.clone({
     headers: req.headers.set('$Set-Cookie', cookie),
-    withCredentials: true
+    withCredentials: true,
+    
   }); 
   return next(req2);
 };
