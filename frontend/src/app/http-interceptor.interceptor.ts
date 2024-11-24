18b3ab9cf78d50f16817584fcec078e08f6addcb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-24 16:05:23 +0000

    Set cookie

--- frontend/src/app/http-interceptor.interceptor.ts
+++ frontend/src/app/http-interceptor.interceptor.ts
@@ -1,10 +1,11 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
-  const userId = sessionStorage.getItem('userId'); 
+  const userId = sessionStorage.getItem('userId');
+  const cookie =  document.cookie = `userId=${userId}`;
 
   const req2 = req.clone({
-    headers: req.headers.set('Set-Cookie',  `userId=${userId}`),
+    headers: req.headers.set('$Set-Cookie', cookie),
     withCredentials: true
   }); 
   return next(req2);
