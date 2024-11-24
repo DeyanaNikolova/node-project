55df2ced159c9767b02510bc8abdcd925360a6e7
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-24 14:43:47 +0000

    Update connection route

--- src/routes/connection/index.ts
+++ src/routes/connection/index.ts
@@ -4,9 +4,11 @@ import path from 'path';
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    
     res.cookie('isAuthenticated', false);
     res.cookie('login', '');
-
+    res.cookie('userId', '');
+    res.send('Cookie has been set');
     res.sendFile(path.join(__dirname, '..', '..', 'views', 'connection.html'));
 });
 
