f62662392e57e79f325ecdcb43417963d4a9cd73
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-24 14:35:55 +0000

    Update user service

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environments';
 import { User } from '../models/user.model';
 import { Observable } from 'rxjs';
