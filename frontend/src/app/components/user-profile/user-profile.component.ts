20fcdeceab667f1f1b6a8e9b5950de82edfca2b9
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-24 12:14:53 +0000

    Update user-profile component

--- frontend/src/app/components/user-profile/user-profile.component.ts
+++ frontend/src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
 
+  user: any;
+ 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUserProfile().subscribe((user)=>{
+      this.user = user;
+    });
+  }
 }
