5c132a46cc1bd2d3034c3d4c4a89f59107764166
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-24 19:23:20 +0000

    Inject user service

--- frontend/src/app/components/user/user.component.ts
+++ frontend/src/app/components/user/user.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
 
+  user: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+
+
+  getUsers(){
+
+  }
 }
