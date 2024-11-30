a0453f937adb2fb49e9856276f9dd6a99358f795
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-11-30 18:28:56 +0000

    Add user-modal component

--- frontend/src/app/components/user/user-modal/user-modal.component.ts
+++ frontend/src/app/components/user/user-modal/user-modal.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-user-modal',
   standalone: true,
@@ -7,24 +9,45 @@ import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angula
   templateUrl: './user-modal.component.html',
   styleUrl: './user-modal.component.css'
 })
-export class UserModalComponent implements OnInit{
+export class UserModalComponent implements OnInit {
 
   firstNameCtrl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]);
-  lastNameCtrl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]); 
-  loginCtrl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]); 
-  roleCtrl = new FormControl('', [Validators.required]);
+  lastNameCtrl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]);
+  loginCtrl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]);
+  roleCtrl = new FormControl('USER', [Validators.required]);
 
   userForm: FormGroup = new FormGroup({
-  firstName: this.firstNameCtrl,
-  lastName: this.lastNameCtrl,
-  login: this.loginCtrl,
-  role: this.roleCtrl
+    firstName: this.firstNameCtrl,
+    lastName: this.lastNameCtrl,
+    login: this.loginCtrl,
+    role: this.roleCtrl
   });
 
   @Input() user: any;
 
-  ngOnInit(): void {
-    
+  constructor(public activeModal: NgbActiveModal) {}
+
+  ngOnInit(): void{
+    if(this.user){
+      this.loginCtrl.setValue(this.user.login);
+      this.loginCtrl.disable();
+      this.firstNameCtrl.setValue(this.user.firstName);
+      this.lastNameCtrl.setValue(this.user.lastName);
+      this.roleCtrl.setValue(this.user.role);
+    }
   }
+
+  submit(){
+    const value = this.userForm.value;
+    this.activeModal.close(value);
+    // const login = this.user ? this.user.login : value.login;
+    // const id = this.user ? this.user.id : null;
+    // this.activeModal.close({...value, id: id, login: login});
+  }
+
+  close(){
+    this.activeModal.close();
+  }
+
 
 }
